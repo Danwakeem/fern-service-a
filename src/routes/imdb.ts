8dd42c47117faa8337074f9f9d9a042bd3d86c5d
Dan Jarvis <danwakeem@gmail.com>
2024-02-27 12:31:36 -0600

    chore: Init submodule

--- src/routes/imdb.ts
+++ src/routes/imdb.ts
@@ -6,7 +6,7 @@ import {
   PutCommand,
 } from "@aws-sdk/lib-dynamodb";
 import { ImdbService } from "../generated/api/resources/imdb/service/ImdbService";
-import { MovieDoesNotExistError } from "../generated/api";
+import { DoesNotExistError } from "../generated/api";
 import { IGetCommandOutput } from "../types/ddb";
 import { Movie } from "../generated/serialization";
 
@@ -15,19 +15,19 @@ const client = new DynamoDBClient();
 const ddb = DynamoDBDocumentClient.from(client);
 
 export const imdb = new ImdbService({
-  getMovie: async ({ params: { id } }, res) => {
+  get: async ({ params: { id } }, res) => {
     const { Item } = (await ddb.send(new GetCommand({
       TableName,
       Key: { id },
     }))) as IGetCommandOutput<Movie.Raw>;
 
     if (!Item) {
-      throw new MovieDoesNotExistError('This movie does not exist');
+      throw new DoesNotExistError('This movie does not exist');
     }
 
     return res.send(Item);
   },
-  createMovie: async (req, res) => {
+  create: async (req, res) => {
     const incomingMovie = req.body;
     const Item = {
       id: randomUUID(),
