7676b404cf9df369ca77b4198038c7f4ef983085
Dan Jarvis <danwakeem@gmail.com>
2024-02-27 12:45:06 -0600

    feat: Added paginated movie endpoint

--- src/routes/imdb.ts
+++ src/routes/imdb.ts
@@ -4,17 +4,34 @@ import {
   DynamoDBDocumentClient,
   GetCommand,
   PutCommand,
+  ScanCommand,
 } from "@aws-sdk/lib-dynamodb";
 import { ImdbService } from "../generated/api/resources/imdb/service/ImdbService";
 import { DoesNotExistError } from "../generated/api";
-import { IGetCommandOutput } from "../types/ddb";
-import { Movie } from "../generated/serialization";
+import { IGetCommandOutput, IScanCommandOutput } from "../types/ddb";
+import { Movie, MoviePagination } from "../generated/serialization";
 
 const TableName = process.env.TABLE_NAME;
 const client = new DynamoDBClient();
 const ddb = DynamoDBDocumentClient.from(client);
 
 export const imdb = new ImdbService({
+  list: async ({ query }, res) => {
+    const { Items, LastEvaluatedKey } = (await ddb.send(new ScanCommand({
+      TableName,
+      Limit: 20,
+      ExclusiveStartKey: JSON.parse(query?.nextToken),
+    }))) as IScanCommandOutput<Movie.Raw>;
+
+    const paginatedResponse: MoviePagination.Raw = {
+      content: Items,
+      meta: {
+        nextToken: JSON.stringify(LastEvaluatedKey),
+      }
+    }
+
+    return res.send(paginatedResponse);
+  },
   get: async ({ params: { id } }, res) => {
     const { Item } = (await ddb.send(new GetCommand({
       TableName,
--- src/types/ddb.ts
+++ src/types/ddb.ts
@@ -1,7 +1,11 @@
-import { GetCommandOutput, PutCommandOutput } from "@aws-sdk/lib-dynamodb";
+import { GetCommandOutput, PutCommandOutput, ScanCommandOutput } from "@aws-sdk/lib-dynamodb";
 
 export type IGetCommandOutput<T> = Omit<GetCommandOutput, "Item"> & {
   Item?: T;
+};
+
+export type IScanCommandOutput<T> = Omit<ScanCommandOutput, "Items"> & {
+  Items?: T[];
 };
 
 export type IPutCommandOutput<T> = Omit<PutCommandOutput, "Attributes"> & {
